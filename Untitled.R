4e9ffbf63147469aaab56b4d43814d0eeaa19b31
Nicole Rodriguez <nicole.rodriguez@rutgers.edu>
2021-08-08 22:41:50 -0400

    chapter 10

--- Untitled.R
+++ Untitled.R
@@ -1,11 +1,11 @@
 # number of students
-x <- 13
+x <- 12
 
 # number of times you want list to run
 r <- 1
 
 # list
-mylist <- c('chanel', 'alausi', 'evelyn', 'gillian', 'dawn', 'carmen', 'maya', 'ryan', 'india', 'rin', 'grace', 'ben', 'kristina')
+mylist <- c('chanel', 'alausi', 'evelyn', 'gillian', 'dawn', 'carmen', 'maya', 'ryan', 'india', 'rin', 'grace', 'ben')
 
 # randomizer
 randommatrix <- matrix(nrow = x, ncol = r)
